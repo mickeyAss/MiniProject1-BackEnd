45aa4e32c8f93a57a3d03776f4348d6a633b31fd
micsarawut <65011212206@msu.ac.th>
2024-07-30 22:53:47 +0700

    edit1

--- api/user.js
+++ api/user.js
@@ -6,7 +6,7 @@ var secret = 'Fullstack-Login-2024';
 
 router.get("/get", async (req, res) => {
     try {
-        conn.query("SELECT * FROM user", (err, result) => {
+        conn.query("SELECT * FROM users", (err, result) => {
             if (err) {
                 console.log(err);
                 return res.status(400).send();
@@ -20,25 +20,25 @@ router.get("/get", async (req, res) => {
 });
 
 router.post("/login", async (req,res) => {
-    const {phone,password} = req.body;
+    const { email , password} = req.body;
 
-    if (phone && password) {
-        conn.query("SELECT * FROM user WHERE phone = ? AND password = ?",
-            [phone,password],
+    if (email && password) {
+        conn.query("SELECT * FROM user WHERE email = ? AND password = ?",
+            [ email , password ],
             (err,result) => {
                 if (err) { 
                     res.json({result: false,message: "Database error"});
                     return;
                 }
                 if (result.length == 0) {
-                    res.json({result: false, message: "Invalid phone or password"});
+                    res.json({result: false, message: "Invalid email or password"});
                     return;
                 }
                 res.json({result: true, result});
             }
         );
     } else {
-        res.json({result: false,message: "Phone and Password are required"});
+        res.json({result: false,message: "Email and Password are required"});
     }
 })
 
