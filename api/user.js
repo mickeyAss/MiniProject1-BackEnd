61770a49e936a81a2128e026c64b248e6c674ca2
micsarawut <65011212206@msu.ac.th>
2024-08-18 21:38:52 +0200

    ddd

--- api/user.js
+++ api/user.js
@@ -12,7 +12,7 @@ router.get("/get/:uid", async (req, res) => {
     const { uid } = req.params; // รับค่า uid จากพารามิเตอร์
 
     try {
-        conn.query("SELECT * FROM users WHERE uid = ?", [uid], (err, result) => {
+        conn.query("SELECT * FROM users_lotto WHERE uid = ?", [uid], (err, result) => {
             if (err) {
                 console.log(err);
                 return res.status(400).json({ error: 'Query error' });
@@ -32,7 +32,7 @@ router.post("/login", async (req, res) => {
     const { phone, password } = req.body;
 
     if (phone && password) {
-        conn.query("SELECT * FROM users WHERE phone = ? AND password = ?",
+        conn.query("SELECT * FROM users_lotto WHERE phone = ? AND password = ?",
             [phone, password],
             (err, result) => {
                 if (err) {
@@ -66,7 +66,7 @@ router.post("/login", async (req, res) => {
 router.post('/register', async (req, res) => {
     const { name, surname, email, password, phone, wallet } = req.body;
 
-    conn.query('INSERT INTO users (name,surname,email ,password,phone,wallet,type) VALUES (?, ?,?,?,?,?,?)',
+    conn.query('INSERT INTO users_lotto (name,surname,email ,password,phone,wallet,type) VALUES (?, ?,?,?,?,?,?)',
         [name, surname, email, password, phone, wallet, "user"],
         function (err, result) {
             if (err) {
@@ -79,54 +79,11 @@ router.post('/register', async (req, res) => {
 });
 
 
-router.put('/update/:uid', async (req, res) => {
-    const { uid } = req.params;
-    const { name, surname, phone, wallet } = req.body;
-
-    // สร้าง array สำหรับเก็บค่าที่จะถูกอัพเดต
-    let fields = [];
-    let values = [];
-
-    // ตรวจสอบและเพิ่ม field ที่ต้องการอัพเดต
-    if (name) {
-        fields.push('name = ?');
-        values.push(name);
-    }
-    if (surname) {
-        fields.push('surname = ?');
-        values.push(surname);
-    }
-    if (phone) {
-        fields.push('phone = ?');
-        values.push(phone);
-    }
-    if (wallet) {
-        fields.push('wallet = ?');
-        values.push(wallet);
-    }
-
-    // ถ้าไม่มี field ใดที่ต้องการอัพเดต ให้ส่ง response กลับโดยไม่ทำอะไร
-    if (fields.length === 0) {
-        return res.json({ result: false, message: 'No fields to update' });
-    }
-
-    // สร้าง query string สำหรับการอัพเดต
-    const query = `UPDATE users SET ${fields.join(', ')} WHERE uid = ?`;
-    values.push(uid);
-
-    conn.query(query, values, function (err, result) {
-        if (err) {
-            return res.json({ result: false, message: err });
-        }
-        res.json({ result: true, message: 'User updated successfully' });
-    });
-});
-
 router.put('/updatewallet/:uid', async (req, res) => {
     const { uid } = req.params;
     const { wallet } = req.body;
 
-    conn.query('UPDATE users SET wallet = ? WHERE uid = ?',
+    conn.query('UPDATE users_lotto SET wallet = ? WHERE uid = ?',
         [wallet, uid],
         function (err, result) {
             if (err) {
